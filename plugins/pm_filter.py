cfd2afb73b38e37bb4ee5ebc24fc88a7f28c7df1
GTZORO <139603543+GTZORO@users.noreply.github.com>
2023-07-29 21:39:44 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -451,15 +451,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             InlineKeyboardButton('⚡ ᴄʟɪᴄᴋ ʜᴇʀᴇ ꜰᴏʀ ᴍᴏʀᴇ ʙᴜᴛᴛᴏɴs ⚡', callback_data='help')
             ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-                
-        await query.message.edit_text(
-        morning_start = 5
-        morning_end = 11
-        afternoon_start = 12
-        afternoon_end = 15
-        evening_start = 16
-        evening_end = 19
-        
         def convert_utc_to_indian(utc_time):
             utc = pytz.timezone('UTC')
             indian = pytz.timezone('Asia/Kolkata')
@@ -467,22 +458,24 @@ async def cb_handler(client: Client, query: CallbackQuery):
             utc_time = utc.localize(utc_time)
             indian_time = utc_time.astimezone(indian)
             return indian_time
-            def get_greeting(indian_time):
-                hour = indian_time.hour
+        
+        def get_greeting(indian_time):
+            hour = indian_time.hour
+            if 5 <= hour < 12:
+                return "Good morning "
+            elif 12 <= hour < 16:
+                return "Good afternoon "
+            elif 16 <= hour < 20:
+                return "Good evening "
+            else:
+                return "Good night "
+        
+        utc_time_str = "2023-07-22 10:30:00"
+        indian_time = convert_utc_to_indian(utc_time_str)
+        greeting = get_greeting(indian_time)
                 
-                if 5 <= hour < 12:
-                    return "Good morning "
-                elif 12 <= hour < 16:
-                    return "Good afternoon "
-                elif 16 <= hour < 20:
-                    return "Good evening "
-                else:
-                    return "Good night "
-                
-                utc_time_str = "2023-07-22 10:30:00"
-                indian_time = convert_utc_to_indian(utc_time_str)
-                greeting = get_greeting(indian_time)
-            text=script.START_TXT.format(greeting, query.from_user.mention, temp.U_NAME, temp.B_NAME),
+        await query.message.edit_text(
+            text=script.START_TXT.format(greeting, query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
        )  
@@ -502,14 +495,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             query.message.id, 
             InputMediaPhoto(random.choice(PICS))
         )
-        await query.message.edit_text(
-        morning_start = 5
-        morning_end = 11
-        afternoon_start = 12
-        afternoon_end = 15
-        evening_start = 16
-        evening_end = 19
-        
         def convert_utc_to_indian(utc_time):
             utc = pytz.timezone('UTC')
             indian = pytz.timezone('Asia/Kolkata')
@@ -517,22 +502,23 @@ async def cb_handler(client: Client, query: CallbackQuery):
             utc_time = utc.localize(utc_time)
             indian_time = utc_time.astimezone(indian)
             return indian_time
-            def get_greeting(indian_time):
-                hour = indian_time.hour
-                
-                if 5 <= hour < 12:
-                    return "Good morning "
-                elif 12 <= hour < 16:
-                    return "Good afternoon "
-                elif 16 <= hour < 20:
-                    return "Good evening "
-                else:
-                    return "Good night "
-                
-                utc_time_str = "2023-07-22 10:30:00"
-                indian_time = convert_utc_to_indian(utc_time_str)
-                greeting = get_greeting(indian_time)
-            text=script.START_TXT.format(greeting, query.from_user.mention, temp.U_NAME, temp.B_NAME),
+        
+        def get_greeting(indian_time):
+            hour = indian_time.hour
+            if 5 <= hour < 12:
+                return "Good morning "
+            elif 12 <= hour < 16:
+                return "Good afternoon "
+            elif 16 <= hour < 20:
+                return "Good evening "
+            else:
+                return "Good night "
+        
+        utc_time_str = "2023-07-22 10:30:00"
+        indian_time = convert_utc_to_indian(utc_time_str)
+        greeting = get_greeting(indian_time)
+        await query.message.edit_text(
+            text=script.START_TXT.format(greeting, query.from_user.mention),
             reply_markup=reply_markup,
             parse_mode=enums.ParseMode.HTML
        )
